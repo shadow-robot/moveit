a6fe5d0a0bcdde4699f67517068c198a5b1ac421
moucrob <ask shadow robot for my email>
2018-12-03 13:55:53 +0000

    same process about conflicts as the previous commit

--- moveit_ros/warehouse/warehouse/src/queries_save_as_text.cpp
+++ moveit_ros/warehouse/warehouse/src/queries_save_as_text.cpp
@@ -45,26 +45,6 @@
 #include <moveit/robot_state/conversions.h>
 #include <ros/ros.h>
 
-<<<<<<< d732d709ac281455bd78221251c6518176b8e0f7
-//#include <moveit/robot_state/robot_state.h>
-#include <eigen_conversions/eigen_msg.h>
-
-static const std::string ROBOT_DESCRIPTION = "robot_description";
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "save_warehouse_as_text", ros::init_options::AnonymousName);
-  
-  boost::program_options::options_description desc;
-  desc.add_options()
-    ("help", "Show help message")("host", boost::program_options::value<std::string>(), "Host for the DB.")
-    ("port", boost::program_options::value<std::size_t>(), "Port for the DB.")
-    ("cartesian", "Save queries defined queries as start state + end effector position instead of full joints goal state.")
-    ("scene", "Saves the scene.")
-    ("eef", boost::program_options::value<std::string>(), "Specify the end effector. Default: last link.");
-  
-=======
-
 static const std::string ROBOT_DESCRIPTION = "robot_description";
 
 typedef std::pair<geometry_msgs::Point, geometry_msgs::Quaternion> LinkConstraintPair;
@@ -103,55 +83,29 @@ int main(int argc, char** argv)
   desc.add_options()("help", "Show help message")("host", boost::program_options::value<std::string>(), "Host for the "
                                                                                                         "DB.")(
       "port", boost::program_options::value<std::size_t>(), "Port for the DB.");
-
->>>>>>> Can now import and save queries directly
+      
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
 
-<<<<<<< d732d709ac281455bd78221251c6518176b8e0f7
-  std::string link_eef = "";
-  if (vm.count("eef"))
-  {
-    link_eef = vm["eef"].as<std::string>();
-  }
-  else
-  {
-    ROS_INFO("If you have a problem with a composite robot, try to use the prefix option.");
-  }
-  
-=======
->>>>>>> Can now import and save queries directly
   if (vm.count("help"))
   {
     std::cout << desc << std::endl;
     return 1;
   }
-<<<<<<< d732d709ac281455bd78221251c6518176b8e0f7
-  
-=======
->>>>>>> Can now import and save queries directly
+
   // Set up db
   warehouse_ros::DatabaseConnection::Ptr conn = moveit_warehouse::loadDatabase();
   if (vm.count("host") && vm.count("port"))
     conn->setParams(vm["host"].as<std::string>(), vm["port"].as<std::size_t>());
   if (!conn->connect())
     return 1;
-<<<<<<< d732d709ac281455bd78221251c6518176b8e0f7
-  
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
-  
-  planning_scene_monitor::PlanningSceneMonitor psm(ROBOT_DESCRIPTION);
-  
-=======
 
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
   planning_scene_monitor::PlanningSceneMonitor psm(ROBOT_DESCRIPTION);
 
->>>>>>> Can now import and save queries directly
   moveit_warehouse::PlanningSceneStorage pss(conn);
   moveit_warehouse::RobotStateStorage rss(conn);
   moveit_warehouse::ConstraintsStorage cs(conn);
@@ -164,142 +118,7 @@ int main(int argc, char** argv)
     moveit_warehouse::PlanningSceneWithMetadata pswm;
     if (pss.getPlanningScene(pswm, scene_names[i]))
     {
-<<<<<<< d732d709ac281455bd78221251c6518176b8e0f7
-      psm.getPlanningScene()->setPlanningSceneMsg(static_cast<const moveit_msgs::PlanningScene&>(*pswm));
-      if(vm.count("scene"))
-      {
-	ROS_INFO("Saving scene '%s'", scene_names[i].c_str());
-	std::ofstream fout((scene_names[i] + ".scene").c_str());
-	psm.getPlanningScene()->saveGeometryToStream(fout);
-	fout.close();
-      }
-      else
-      {
-	std::vector<std::string> robotStateNames;
-	robot_model::RobotModelConstPtr km = psm.getRobotModel();
-	// Get start states for scene
-	std::stringstream rsregex;
-	rsregex << ".*" << scene_names[i] << ".*";
-	rss.getKnownRobotStates(rsregex.str(), robotStateNames);
-      
-	// Get goal constraints for scene
-	std::vector<std::string> constraintNames;
 
-	std::stringstream csregex;
-	csregex << ".*" << scene_names[i] << ".*";
-	cs.getKnownConstraints(csregex.str(), constraintNames);
-
-	
-	std::vector<std::string> query_names;
-
-	std::stringstream pssregex;
-	pssregex << ".*";
-	pss.getPlanningQueriesNames(pssregex.str(), query_names, scene_names[i]);
-
-	std::string prefix = "";
-	if(vm.count("cartesian"))
-	{
-	  prefix = "cartesian_";
-	}
-	
-	std::ofstream qfout((prefix + scene_names[i] + ".queries").c_str());
-	qfout << scene_names[i] << std::endl;
-	
-	for (std::size_t k = 0; k < query_names.size(); ++k)
-	{
-	  
-	  ROS_INFO("Saving query '%s' from scene '%s'", query_names[k].c_str(), scene_names[i].c_str());
-	  
-	  moveit_warehouse::MotionPlanRequestWithMetadata planning_query;
-	  try
-	  {
-	    pss.getPlanningQuery(planning_query, scene_names[i], query_names[k]);
-	  }
-	  catch (std::exception& ex)
-	  {
-	    ROS_ERROR("Error loading motion planning query '%s': %s", query_names[i].c_str(), ex.what());
-	    continue;
-	  }
-	  
-	  moveit_msgs::MotionPlanRequest plan_request = static_cast<moveit_msgs::MotionPlanRequest>(*planning_query);
-	  std::vector<moveit_msgs::Constraints> query_goal_constraints = plan_request.goal_constraints;
-	  
-	  if (query_goal_constraints.size() == 1)
-	  {
-	    moveit_msgs::Constraints query_goal = query_goal_constraints[0];
-	    moveit_msgs::RobotState startState = plan_request.start_state;
-	    sensor_msgs::JointState jointState = startState.joint_state;
-	    
-	    std::vector<moveit_msgs::JointConstraint> joint_constraints = query_goal.joint_constraints;
-	    std::vector<moveit_msgs::PositionConstraint> position_constraints = query_goal.position_constraints;
-	    std::vector<moveit_msgs::OrientationConstraint> orientation_constraints = query_goal.orientation_constraints;
-	    
-	    if(joint_constraints.size() != 0)  //save only joint_defined queries
-	    {
-	      if(vm.count("cartesian")){  //allows to distinguish queries defined by position and by joints
-		query_names[k] = "cartesian_" + query_names[k];
-	      }
-	      qfout << query_names[k] << std::endl;
-	      
-	      //Save the start State
-	      qfout << "START" << std::endl;
-	      for (std::size_t p = 0; p < jointState.position.size(); ++p){
-		qfout << jointState.name[p] << " = ";
-		qfout << jointState.position[p] << std::endl;
-	      }
-	      qfout << "." << std::endl;
-	      
-	      //Save the goal state
-	      qfout << "GOAL" << std::endl;
-	      if(vm.count("cartesian"))  //save queries using end-effector position
-	      {
-		qfout << "position_constraint" << std::endl;
-		
-		moveit::core::RobotState goal_state(km);
-		for(int i = 0; i<joint_constraints.size(); ++i)
-		{
-		  goal_state.setJointPositions(joint_constraints[i].joint_name, {joint_constraints[i].position});
-		}
-		
-		const moveit::core::JointModel* joint_eef = goal_state.getJointModel(joint_constraints[joint_constraints.size()-1].joint_name);
-		
-		if(link_eef == "")
-		  link_eef = joint_eef->getChildLinkModel()->getName();
-
-		const Eigen::Affine3d& link_pose = goal_state.getGlobalLinkTransform(link_eef);
-		
-		geometry_msgs::Transform transform;
-		tf::transformEigenToMsg(link_pose, transform);
-		
-		qfout << "Position =";
-		qfout << " " << transform.translation.x;
-		qfout << " " << transform.translation.y;
-		qfout << " " << transform.translation.z << std::endl;
-	        qfout << "Orientation =";
-		qfout << " " << transform.rotation.x;
-		qfout << " " << transform.rotation.y;
-		qfout << " " << transform.rotation.z;
-		qfout << " " << transform.rotation.w << std::endl;
-	        qfout << "." << std::endl;
-	      }
-	      else  //save queries using joint constraints
-	      {
-		qfout << "joint_constraint" << std::endl;
-		std::vector<moveit_msgs::JointConstraint> joint_constraints = query_goal.joint_constraints;
-		if(joint_constraints.size() != 0)
-		{
-		  for(auto iter = joint_constraints.begin(); iter != joint_constraints.end(); iter++)
-		  {
-		    qfout << iter->joint_name << " = ";
-		    qfout << iter->position << std::endl;//" ";
-		  }
-		}
-		qfout << "." << std::endl;
-	      }
-	    }
-	  }
-	}
-=======
       ROS_INFO("Saving scene '%s'", scene_names[i].c_str());
       psm.getPlanningScene()->setPlanningSceneMsg(static_cast<const moveit_msgs::PlanningScene&>(*pswm));
       std::ofstream fout((scene_names[i] + ".scene").c_str());
@@ -380,16 +199,10 @@ int main(int argc, char** argv)
 	      }
 	    
 	  }
->>>>>>> Can now import and save queries directly
 	qfout.close();
       }
     }
   }
-<<<<<<< d732d709ac281455bd78221251c6518176b8e0f7
-  
-=======
-
->>>>>>> Can now import and save queries directly
   ROS_INFO("Done.");
 
   return 0;
