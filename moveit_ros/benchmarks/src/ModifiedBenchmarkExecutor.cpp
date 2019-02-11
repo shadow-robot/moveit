ca5066c50cef9deb892182a0b273eabf7ce87a1d
moucrob <mathiasmouchrik@gmail.com>
2019-02-11 20:38:21 +0000

    getEndEffectorTips only work for parallel robots

--- moveit_ros/benchmarks/src/ModifiedBenchmarkExecutor.cpp
+++ moveit_ros/benchmarks/src/ModifiedBenchmarkExecutor.cpp
@@ -1545,18 +1545,21 @@ void ModifiedBenchmarkExecutor::runBenchmark(moveit_msgs::MotionPlanRequest requ
 							visual_tools_->trigger();
 			
 							//trajectory markers
-							//visual_tools_->publishTrajectoryLine(first_mp_res.trajectory_.back(), joint_model_group, traj_rope_color_orig);
+							visual_tools_->publishTrajectoryLine(first_mp_res.trajectory_.back(), joint_model_group, traj_rope_color_orig);
 							
 							//test : trial to plot each path linking each joints of the start/goal conf
 							// http://docs.ros.org/kinetic/api/moveit_visual_tools/html/moveit__visual__tools_8cpp_source.html#l01327
 							std::vector<const moveit::core::LinkModel*> tips;
 							joint_model_group->getEndEffectorTips(tips);
+							ROS_ERROR("[DEBUG] tips.size() = %lu", tips.size());
+							//Conclusion this does only work for parallel robots
 							// For each end effector
 							for (const moveit::core::LinkModel* ee_parent_link : tips)
 							{
 								visual_tools_->publishTrajectoryLine(first_mp_res.trajectory_.back(), ee_parent_link, traj_rope_color_orig);
-								//visual_tools_->trigger();
+								visual_tools_->trigger();
 							}
+							//end test
 							
 							visual_tools_->trigger(); //forces a refresh with the new trajectory markers
 							ROS_INFO("(Originally parametrized planner solution EE path gave this)");
