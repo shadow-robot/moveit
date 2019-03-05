334653f7556674f4a886bfebfd0f5e81fafac378
moucrob <me@me.com>
2019-03-05 00:03:20 +0000

    typing error

--- moveit_ros/benchmarks/src/ModifiedBenchmarkExecutor.cpp
+++ moveit_ros/benchmarks/src/ModifiedBenchmarkExecutor.cpp
@@ -1702,7 +1702,7 @@ void ModifiedBenchmarkExecutor::runBenchmark(moveit_msgs::MotionPlanRequest requ
 						  if (RECORD_ANIMATION_RVIZ)
 						  { //start recording
 						  	std::string str = PATH_TO_EXECUTABLE_BASHSCRIPT , sep = "__"; 
-    						str = str + JOINT_ANGLE_STATUS + sep + "countdown" + std::to_string(countdown) + sep + queryName + sep + planner + sep + metricChoice + sep + "acceptance_" + acceptanceFuncExpressionForFileName + sep + "metric_" + std::to_string((previousPlanQuality-firstPlanQuality)*100.) + sep + "run_" + std::to_string(j+1); 
+    						str = str + JOINT_ANGLE_STATUS + sep + "countdown" + std::to_string(countdown) + sep + queryName + sep + planner + sep + metricChoice + sep + "acceptance_" + acceptanceFuncExpressionForFileName + sep + "metric_" + std::to_string((previousPlanQuality-first_planQuality)*100.) + sep + "run_" + std::to_string(j+1); 
 						  	const char *command = str.c_str(); 
    							ROS_ERROR("[DEBUG] (Requesting shell to do : %s)", command); 
     						std::system(command); 
